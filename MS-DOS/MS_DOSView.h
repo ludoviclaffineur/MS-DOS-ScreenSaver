d5d78e7994363bb8dbdcf83e3c5618662b9f422f
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2015-06-23 09:37:52 +0200

    Initial Commit

---
+++ MS-DOS/MS_DOSView.h
@@ -0,0 +1,13 @@
+//
+//  MS_DOSView.h
+//  MS-DOS
+//
+//  Created by Ludovic Laffineur on 23/06/15.
+//  Copyright (c) 2015 Ludovic Laffineur. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MS_DOSView : ScreenSaverView
+
+@end
---
+++ MS-DOS/MS_DOSView.m
@@ -0,0 +1,52 @@
+//
+//  MS_DOSView.m
+//  MS-DOS
+//
+//  Created by Ludovic Laffineur on 23/06/15.
+//  Copyright (c) 2015 Ludovic Laffineur. All rights reserved.
+//
+
+#import "MS_DOSView.h"
+
+@implementation MS_DOSView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
